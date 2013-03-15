5f53e1a71dc95561af0e8ebe31e6eee935c9ba10
Brendan Molloy <brendan@bbqsrc.net>
2013-03-15 18:18:43 +1100

    setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+setup(name='simplebdd',
+      version='0.1',
+      description='Simple Behaviour Driven Development Framework',
+      author='Brendan Molloy',
+      author_email='brendan@bbqsrc.net',
+      url='https://github.com/bbqsrc/simplebdd',
+      packages=['simplebdd'],
+      scripts=['scripts/simplebdd']
+     )
