588554bdcf468711870aa6079f091fc3f93c0786
Brendan Molloy <brendan@bbqsrc.net>
2013-03-16 23:50:08 +1100

    version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='simplebdd',
-      version='0.1',
+      version='0.2',
       description='Simple Behaviour Driven Development Framework',
       author='Brendan Molloy',
       author_email='brendan@bbqsrc.net',
